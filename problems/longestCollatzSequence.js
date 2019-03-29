43d00bc9f71b7e6de59a84556b365b236f3dcd63
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-03-29 14:58:31 -0600

    Update longestCollatzSequence.js

--- problems/longestCollatzSequence.js
+++ problems/longestCollatzSequence.js
@@ -1,20 +1,28 @@
-function createCollatz(num, arr) {
-  arr.push(num)
-  if (num%2 === 0) {
-    num = num /2
-  } else {
-    num = 3*num + 1
+function createCollatz(num, count) {
+  while (num>1) {
+    count = count + 1
+    if (num%2 === 0) {
+      num = num / 2
+    } else {
+      num = 3*num + 1
+    }
   }
-  createCollatz(num, arr)
-  return arr
+  return count
 }
 
 function longestCollatzSequence(limit) {
-  var arr = []
-  for(let i=0; i<limit; i++) {
-    arr = createCollatz(i, arr)
-    console.log(arr)
+  var c = 0
+  var maxChain = 0
+  var max
+  for (let i=0; i<100000; i++) {
+    var chain = createCollatz(i, c)
+    if (chain > maxChain) {
+      maxChain = chain
+      max = i
+    }
   }
+  console.log(max)
+  return max
   
 }
 
