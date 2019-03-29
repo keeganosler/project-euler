551510af697236eca0a37f44777b085b46d455a9
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-03-29 09:37:55 -0600

    Update specialPythagoreanTriplet.js

--- problems/specialPythagoreanTriplet.js
+++ problems/specialPythagoreanTriplet.js
@@ -1,21 +1,22 @@
  //Pythagoras: (a^2) + (b^2) = (c^2)
- //Also: a + b + c = 1000
+ //Also: a + b + c = n
  //Rearrange for c and sub this into Pythagoras which gives the following:
- // a = (500000 - 1000*b) / (1000 - b)
+ // a = (0.5*(n^2) - n*b) / (n - b)
 
 function specialPythagoreanTriplet(n) {
     let sumOfabc = n;
     var a, c
-    for (var b=1; b<1000; b++) {
-        a = (500000-1000*b)/(1000-b)
+    for (var b=1; b<n; b+=1) {
+        a = ((n^2)-(2*n*b))/(2*n-2*b)
         if (Math.floor(a)===a) {
-            c = 1000-a-b
+            c = n-a-b
             break
         }
     }
     sumOfabc = a*b*c
-
+    console.log(sumOfabc)
     return sumOfabc;
+    
 }
 
 specialPythagoreanTriplet(1000);
