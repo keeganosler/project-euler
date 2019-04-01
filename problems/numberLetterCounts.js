f4c2dcaecd704a5e6e8b68f90e2f86380e849205
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-01 10:57:57 -0600

    Create numberLetterCounts.js

---
+++ problems/numberLetterCounts.js
@@ -0,0 +1,7 @@
+function numberLetterCounts(limit) {
+  var nums = ['one','two','three','four','five','six','seven','eight','nine']
+  var teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','ninteen']
+  var tens = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+}
+
+numberLetterCounts(5);
