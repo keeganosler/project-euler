b9261b673f97234465b3fcfc3df2bdeee56262ab
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-01 13:45:53 -0600

    Update numberLetterCounts.js

--- problems/numberLetterCounts.js
+++ problems/numberLetterCounts.js
@@ -2,6 +2,23 @@ function numberLetterCounts(limit) {
   var nums = ['one','two','three','four','five','six','seven','eight','nine']
   var teens = ['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','ninteen']
   var tens = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+  var word = []
+  var sum = 0
+  for (let i=0; i<limit; i++) {
+    if (i <= 9) {
+      word.push(nums[i])
+    } else if (i >= 10 && i< 20) {
+      word.push(teens[i])
+    } else {
+      word.push(tens[i])
+    }
+  }
+ for (let i=0; i<word.length; i++) {
+   var thisWord = word[i]
+   sum = sum + thisWord.length
+ }
+ return sum
+   
 }
 
 numberLetterCounts(5);
