00a8b602447b118b43ad2777d5e00619bfdb07e4
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-02 12:24:32 -0600

    Update namesScores.js

--- problems 20-39/namesScores.js
+++ problems 20-39/namesScores.js
@@ -1,15 +1,16 @@
 function namesScores(arr) {
   var sortedArr = arr.sort()
+  var totalSum = 0
   for (let i=0; i<sortedArr.length; i++) {
-    var alphSum=0
+    var alphSum = 0
     for (let j=0; j<sortedArr[i].length; j++) {
-      alphSum = alphSum + sortedArr[i].charCodeAt(j)-64
-      console.log(alphSum)
+      alphSum = alphSum + sortedArr[i].charCodeAt(j)-64 
     }
+    totalSum = totalSum + alphSum*(i+1)
   }
+  return totalSum
 }
 
-// Only change code above this line
 const test1 = ['THIS', 'IS', 'ONLY', 'A', 'TEST'];
 const test2 = ['I', 'REPEAT', 'THIS', 'IS', 'ONLY', 'A', 'TEST'];
 
