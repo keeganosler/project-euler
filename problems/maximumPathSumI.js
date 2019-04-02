444ca161a9b9fbc2f13ac4185e885db218dc79e7
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-02 11:14:22 -0600

    Update maximumPathSumI.js

--- problems/maximumPathSumI.js
+++ problems/maximumPathSumI.js
@@ -1,18 +1,8 @@
 function maximumPathSumI(triangle) {
   var maxSum = 0
-  /*for (let i=0; i<triangle.length; i++) {
-    if (i===0) {
-      maxSum = maxSum + triangle[0][0]
-    } 
-    console.log('i', i)
-    maxSum = maxSum + Math.max(triangle[i][i], triangle[i][i+1])
-    console.log('sum', maxSum)
-  }*/
   maxSum = maxSum + triangle[0][0] + Math.max(triangle[1][0], triangle[1][1])
-  console.log(maxSum)
   for (let i=2; i<triangle.length; i++) {
     maxSum = maxSum + Math.max(triangle[i][i-1], triangle[i][i-2])
-    console.log(maxSum)
   }
   return maxSum
 }
