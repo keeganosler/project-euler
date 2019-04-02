1b46245a66b1fae4b2c3edf7d490348e634bcee6
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-02 09:14:51 -0600

    Update numberLetterCounts.js

--- problems/numberLetterCounts.js
+++ problems/numberLetterCounts.js
@@ -7,12 +7,14 @@ function numberLetterCounts(limit) {
   for (let i=0; i<limit; i++) {
     if (i <= 9) {
       word.push(nums[i])
-    } else if (i >= 10 && i< 20) {
-      word.push(teens[i])
+    } else if (i >= 10 && i < 20) {
+      word.push(teens[i-10])
+      console.log(word)
     } else {
       word.push(tens[i])
     }
   }
+
  for (let i=0; i<word.length; i++) {
    var thisWord = word[i]
    sum = sum + thisWord.length
